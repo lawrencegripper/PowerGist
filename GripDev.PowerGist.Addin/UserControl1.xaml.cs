1229196831b53d42930b424aacddc0bc099abfc0
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-01-13 12:28:54 +0000

    Working gist integration with login to github. Very basic.

--- GripDev.PowerGist.Addin/UserControl1.xaml.cs
+++ GripDev.PowerGist.Addin/UserControl1.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.PowerShell.Host.ISE;
+﻿using GistsApi;
+using Microsoft.PowerShell.Host.ISE;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,9 +23,41 @@ namespace GripDev.PowerGist.Addin
     /// </summary>
     public partial class UserControl1 : IAddOnToolHostObject
     {
+        private GistClient gistClient;
+
         public UserControl1()
         {
             InitializeComponent();
+
+            gistClient = new GistClient("1eb530bea98d9f863c57", "1e55daaec72d64581f8688e7bbb3e779c83b3262", "powershellISEAddin");
+
+            //navigate to "https://github.com/login/oauth/authorize" 
+            webBrowser.Navigate(gistClient.AuthorizeUrl);
+        }
+
+        private async void webBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            if (e.Uri == null)
+            { return; }
+
+            if (e.Uri.AbsoluteUri.Contains("code="))
+            {
+                var authCode = Regex.Split(e.Uri.AbsoluteUri, "code=")[1];
+
+                //get access token
+                await gistClient.Authorize(authCode);
+
+                webBrowser.Visibility = Visibility.Collapsed;
+
+                var gists = await gistClient.ListGists(GistClient.ListMode.AuthenticatedUserGist);
+
+                foreach (var g in gists)
+                {
+                    listBox.Items.Add(g.files.Select(x => x.filename));
+                }
+            }
+
+
         }
 
         public ObjectModelRoot HostObject { get; set; }
