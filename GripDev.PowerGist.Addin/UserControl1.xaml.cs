70362ff6aa0f67245fbcd543d45e74a6330df967
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-01-13 12:19:47 +0000

    Started working on ISE addin for Gist

---
+++ GripDev.PowerGist.Addin/UserControl1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.PowerShell.Host.ISE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GripDev.PowerGist.Addin
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : IAddOnToolHostObject
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public ObjectModelRoot HostObject { get; set; }
+    }
+}
