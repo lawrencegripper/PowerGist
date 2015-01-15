1d8ae6935a14a4a075b5eecb3420a75bba84d67d
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-01-15 18:44:50 +0000

    Tweaked config

---
+++ GripDev.PowerGist.Addin/GitApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GripDev.PowerGist.Addin
+{
+    class GitApiConfig
+    {
+        private const string unconfiguredConstant = "ReplaceMe";
+        static GitApiConfig()
+        {
+
+        }
+
+        public static void AlertIfConfigRequired()
+        {
+            if (ClientId == unconfiguredConstant)
+            {
+                MessageBox.Show("ClientID and Secret require configuration, see GitApiConfig class");
+            }
+        }
+
+        public static string ClientId = unconfiguredConstant;
+        public static string ClientSecret = unconfiguredConstant;
+
+    }
+}
