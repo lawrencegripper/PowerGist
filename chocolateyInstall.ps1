b99e8eb689a241775a3d126c74c4a6892e2492fb
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-01-21 09:29:50 +0000

    Changes for Choco package moderation.

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $path = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $pathLib = "$path\..\lib\"
 
-Write-host $pathLib
+Write-Debug $pathLib
 
 $addinDll = "$pathLib\GripDev.PowerGist.Addin.dll"
 $depDll = "$pathLib\GistsApi.dll"
@@ -14,8 +14,8 @@ $iseProfile = "$pshome\Microsoft.PowerShellISE_profile.ps1"
 Copy-Item $addinDll $pshome -Force 
 Copy-Item $depDll $pshome -Force 
 
-$line1 = "Add-Type -Path '$pshome\GripDev.PowerGist.Addin.dll'“
-$line2 = "Add-Type -Path '$pshome\GistsApi.dll'“
+$line1 = "Add-Type -Path '$pshome\GripDev.PowerGist.Addin.dll'"
+$line2 = "Add-Type -Path '$pshome\GistsApi.dll'"
 $line3 = "`$psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('PowerGist', [GripDev.PowerGist.Addin.PowerGistPanel], `$true) "
 
 $profile = Get-Content $iseProfile
@@ -28,12 +28,12 @@ if (-not $profile)
 }
 else
 {
-	Write-Host "*****************************************************"
-	Write-Host "ISE Profile configuration already setup, manuall add the following to: " + $iseProfile
-	Write-Host $line1
-	Write-host $line2
-	Write-host $line3
-	Write-Host "*****************************************************"
+	Write-Debug "*****************************************************"
+	Write-Debug "ISE Profile configuration already setup, manuall add the following to: " + $iseProfile
+	Write-Debug $line1
+	Write-Debug $line2
+	Write-Debug $line3
+	Write-Debug "*****************************************************"
 }
 
 Write-ChocolateySuccess $packageName
