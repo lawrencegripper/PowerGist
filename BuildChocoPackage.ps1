6c39031cc27d2aceb46d0285f2b032444bc1f96b
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-01-21 17:45:53 +0000

    Working Postbuild event to package nuget package from nuspec automatically.

--- BuildChocoPackage.ps1
+++ BuildChocoPackage.ps1
@@ -1,1 +1,4 @@
 ﻿.\packages\NuGet.CommandLine.2.8.3\tools\nuget.exe pack .\PowerGist.Install.1.0.0.nuspec
+
+#For post build event using this adapted
+#"$(TargetDir)..\..\..\packages\NuGet.CommandLine.2.8.3\tools\nuget.exe" pack "$(TargetDir)\..\..\..\Powergist.nuspec"
