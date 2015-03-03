01e9677918f744518ddcca1b38694daa326735b2
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-03-03 17:16:18 +0000

    Tweaks for choco publish.

--- BuildChocoPackage.ps1
+++ BuildChocoPackage.ps1
@@ -1,4 +1,4 @@
-﻿.\packages\NuGet.CommandLine.2.8.3\tools\nuget.exe pack .\PowerGist.Install.1.0.0.nuspec
+﻿.\packages\NuGet.CommandLine.2.8.3\tools\nuget.exe pack .\powergist.nuspec
 
 #For post build event using this adapted
 #"$(TargetDir)..\..\..\packages\NuGet.CommandLine.2.8.3\tools\nuget.exe" pack "$(TargetDir)\..\..\..\Powergist.nuspec"
